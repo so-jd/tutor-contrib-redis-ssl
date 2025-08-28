f1912299db8cef66fd74b7da23e716d2179bbb78
Snir Orlanczyk <sorlanczyk@jacksondawson.com>
2025-08-28 16:10:46 -0400

    add override

--- tutorredis_ssl/plugin.py
+++ tutorredis_ssl/plugin.py
@@ -159,17 +159,3 @@ for path in glob(str(importlib_resources.files("tutorredis_ssl") / "patches" / "
     with open(path, encoding="utf-8") as patch_file:
         hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
 
-
-hooks.Filters.ENV_PATCHES.add_item(
-    (
-        "openedx-common-settings",
-        """# Update all Redis cache connections to use SSL
-for cache_name, cache_config in CACHES.items():
-    if cache_config.get('BACKEND') == 'django_redis.cache.RedisCache':
-        # Change redis:// to rediss://
-        location = cache_config['LOCATION']
-        if location.startswith('redis://'):
-            cache_config['LOCATION'] = location.replace('redis://', 'rediss://', 1)
-""",
-    )
-)
