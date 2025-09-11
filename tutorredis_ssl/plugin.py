9912de34c997067a7a9c9bc38da4252a5d517e2f
Snir Orlanczyk <sorlanczyk@jacksondawson.com>
2025-09-11 11:52:49 -0400

    update

--- tutorredis_ssl/plugin.py
+++ tutorredis_ssl/plugin.py
@@ -1,7 +1,6 @@
 from tutor import hooks
 
-hooks.Filters.CONFIG_DEFAULTS.add_item(
-    [
-        ("REDIS_TRANSPORT", "redis"),
-    ]
-)
+@hooks.Filters.CONFIG_DEFAULTS.add()
+def _add_redis_transport_default(items):
+    items.append(("REDIS_TRANSPORT", "redis"))
+    return items
