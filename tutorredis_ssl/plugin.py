57a570d0653b335faab39fd547f8590c31e740e4
Snir Orlanczyk <sorlanczyk@jacksondawson.com>
2025-09-11 12:18:25 -0400

    update

--- tutorredis_ssl/plugin.py
+++ tutorredis_ssl/plugin.py
@@ -1,6 +1,43 @@
-from tutor import hooks
+from __future__ import annotations
+from glob import glob
+import os
+import pkg_resources
+import uuid
 
-@hooks.Filters.CONFIG_DEFAULTS.add()
-def _add_redis_transport_default(items):
-    items.append(("REDIS_TRANSPORT", "redis"))
-    return items
+from tutor import hooks as tutor_hooks
+
+from .__about__ import __version__
+
+########################################
+# CONFIGURATION
+########################################
+
+tutor_hooks.Filters.CONFIG_DEFAULTS.add_item(
+    ("REDIS_TRANSPORT", "redis")
+)
+
+########################################
+# INITIALIZATION TASKS
+########################################
+
+
+########################################
+# TEMPLATE RENDERING
+########################################
+
+
+########################################
+# PATCH LOADING
+########################################
+
+# For each file in tutor_media/patches,
+# apply a patch based on the file's name and contents.
+patch_files = glob(
+    os.path.join(
+        pkg_resources.resource_filename("tutorredis_ssl", "patches"),
+        "*",
+    )
+)
+for path in patch_files:
+    with open(path, encoding="utf-8") as patch_file:
+        tutor_hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
